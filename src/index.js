1337133b5eba5b72de125cbf32a3dc9844beea5a
jru <jrubio.iradi@gmail.com>
2018-04-23 21:26:28 +0200

    Refactor fontawesome import

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import fontawesome from '@fortawesome/fontawesome'
-import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
-import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
-import faRedo from '@fortawesome/fontawesome-free-solid/faRedo';
-import faHeart from '@fortawesome/fontawesome-free-solid/faHeart';
+import { faTimes, faCheck, faRedo, faHeart } from '@fortawesome/fontawesome-free-solid';
 
 fontawesome.library.add(faTimes, faCheck, faRedo, faHeart);
 
