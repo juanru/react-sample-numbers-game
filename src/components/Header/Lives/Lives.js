39398ef88c8959f6ee06e9b24c9102914dc5730d
jru <jrubio.iradi@gmail.com>
2018-04-23 21:28:45 +0200

    Force import again to prevent error "Icon not found" by unit testing (even if it is not a real error)

--- src/components/Header/Lives/Lives.js
+++ src/components/Header/Lives/Lives.js
@@ -2,13 +2,14 @@ import React from 'react';
 import _ from "lodash";
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {faHeart} from "@fortawesome/fontawesome-free-solid";
 
 const Lives = (props) => {
     const {remainingLives} = props;
     return (
         <div>Remaining lives:&nbsp;
             {_.times(remainingLives, function (i) {
-                return <FontAwesomeIcon key={i} icon="heart" className={'remainingLive'}/>
+                return <FontAwesomeIcon key={i} icon={faHeart} className={'remainingLive'}/>
             })}
         </div>
     );
